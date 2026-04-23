b3382676702110eaae42c151f13767407b252d83
arpita0212 <arpita@pipra.solutions>
2026-04-23 10:52:25 +0530

    List methods

---
+++ List.py
@@ -0,0 +1,24 @@
+myList = ["apple", "banana", "cherry"]
+
+#  print the list
+print(myList)
+
+#  allow duplicates
+
+thisList = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thisList)
+
+print(len(thisList))  # length of the list
+
+# can contain different data types
+list1 = ["abc", 34, True, 40, "female"]
+
+print(list1)
+
+# find the data type
+myList1 = ["apple", "banana", "cherry"]
+print(type(myList1))
+
+#list constructor
+thisList1 = list(("apple", "banana", "cherry")) # double round-brackets
+print(thisList1)
